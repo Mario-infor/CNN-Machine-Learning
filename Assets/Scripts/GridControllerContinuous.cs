d859f7930038a367db2b5f2685e781f252552a26
MarioDev <mherreraalmira@gmail.com>
2023-10-18 02:03:02 -0600

    change actions to float array

--- Assets/Scripts/GridControllerContinuous.cs
+++ Assets/Scripts/GridControllerContinuous.cs
@@ -28,7 +28,6 @@ public class GridControllerContinuos : MonoBehaviour
     [SerializeField] private int episodes = 1000;
 
     private TileState[,] gridPosMatrix;
-    private string[] actions = { "up", "right", "down", "left" };
     private float[,] actionsDiscreet = {{-1f, 0f}, {0f, 1f}, {1f, 0f}, {0f, -1f} };
     private bool startTraining = false;
     private float winsCount = 0f;
