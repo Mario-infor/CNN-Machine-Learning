a1f34caea9750729cd22909601d573bfcaad9cb2
MarioDev <mherreraalmira@gmail.com>
2023-10-30 15:05:56 -0600

    Add comments to GridControllerContinuous

--- Assets/Scripts/GridControllerContinuous.cs
+++ Assets/Scripts/GridControllerContinuous.cs
@@ -85,32 +85,37 @@ public class GridControllerContinuos : MonoBehaviour
             xCenterFlag++;
         }
 
+
         gaussCount = centers.Count;
         gaussArray = new GaussianSurfaceClass[actionsDiscreet.GetLength(0)];
 
+        // Initialize guassian surfaces and its corresponding centers list.
         for (int i = 0; i < actionsDiscreet.GetLength(0); i++)
         {
             gaussArray[i] = new GaussianSurfaceClass(gaussCount, tranningIte);
             listCentersTileList.Add(new List<GameObject>());
         }
 
+        // Check if gaussian centers shuold be display at runtime or not.
         if (showGaussiansUI)
         {
             for (int i = 0; i < listCentersTileList.Count; i++)
             {
                 for (int j = 0; j < centers.Count; j++)
                 {
-                    listCentersTileList[i].Add(createGaussCenterTile(centers[j][0], centers[j][1], gaussArray[i].WList[j], GaussTile));
+                    listCentersTileList[i].Add(createGaussCenterCube(centers[j][0], centers[j][1], gaussArray[i].WList[j], GaussTile));
                 }
             }
         }
 
+        // Initial setup (goal tile position, move player to goal tile).
         getStartingLocation(out randomGoalX, out randomGoalY);
         createTile(randomGoalX, randomGoalY, GoalTile);
         movePlayer(randomGoalX, randomGoalY);
         goalPos.x = randomGoalX;
         goalPos.y = randomGoalY;
 
+        // Check if player should start on the same tile every epoch or if it should be random positions.
         if (startRandomEachEpisode)
             StartCoroutine(TrainQLearningStartRandom());
         else
@@ -168,17 +173,20 @@ public class GridControllerContinuos : MonoBehaviour
         return actionsDiscreet;
     }
 
+    // Start trainning.
     public void play()
     {
         startTraining = true;
     }
 
+    // Check if player went off the terrain or if it reached the goal.
     private bool isTerminalState(float x, float y)
     {
         Vector3Int position = tilemap.WorldToCell(new Vector3(x, y, 0));
         return !tilemap.GetTile(position) || (x == goalPos.x && y == goalPos.y);
     }
 
+    // Get a random starting point inside the terrain and different to the goal position.
     private void getStartingLocation(out float X, out float Y)
     {
         X = UnityEngine.Random.Range(bounds.xMin, bounds.xMax);
@@ -191,6 +199,8 @@ public class GridControllerContinuos : MonoBehaviour
         }
     }
 
+    // Choose the next move the player should do. If a random value between 0 and 1 es greater than
+    // epsilon then choose the best move, else choose a random move.
     private int getNextAction(float x, float y, float epsilon)
     {
         int index = -1;
@@ -218,30 +228,38 @@ public class GridControllerContinuos : MonoBehaviour
         return index;
     }
 
+    // Get the next position of the player based on the current position plus the selected move.
     private void getNextLocation(float x, float y, int action, out float newX, out float newY)
     {
         newX = x + actionsDiscreet[action, 0];
         newY = y + actionsDiscreet[action, 1];
     }
 
+    // Instantiate a given tile prefab on a given location.
     private GameObject createTile(float x, float y, GameObject tile)
     {
         Vector3 pos = new Vector3(x + 0.5f, y + 0.5f);
         return Instantiate(tile, pos, Quaternion.identity);
     }
 
-    private GameObject createGaussCenterTile(float x, float y, float z, GameObject tile)
+    // Instatiate a gaussian center to be display on runtime.
+    private GameObject createGaussCenterCube(float x, float y, float z, GameObject cube)
     {
         Vector3 pos = new Vector3(x, y, z);
-        return Instantiate(tile, pos, Quaternion.identity);
+        return Instantiate(cube, pos, Quaternion.identity);
     }
 
+    // Move the player to a given location.
     private void movePlayer(float x, float y)
     {
         Vector3 pos = new Vector3(x + 0.5f, y + 0.5f);
         player.transform.position = pos;
     }
 
+    // Check the position of the player and get the corresponding reward.
+    // Outside the map get -100.
+    // On goal tile get +100.
+    // Any other case get -1.
     private int GetReward()
     {
         int reward = -1;
@@ -255,11 +273,13 @@ public class GridControllerContinuos : MonoBehaviour
         return reward;
     }
 
+    // Calculate the value of evaluating a coordenate in a gaussian surface.
     private float GetQValue(float x, float y, int actionIndex)
     {
         return gaussArray[actionIndex].calculateH(x, y, sigma, centers);
     }
 
+    // Get the best evaluation of a coordenate in all gaussian surfaces.
     private float GetQValueMax(float x, float y)
     {
         float best = -100f;
@@ -274,27 +294,33 @@ public class GridControllerContinuos : MonoBehaviour
         return best;
     }
 
+    // Trainning process starting always on the same tile each epoch.
     IEnumerator TrainQLearningStartFix()
     {
         while (true)
         {
+            // When play btton is pressed.
             if (startTraining)
             {
                 float startX;
                 float startY;
 
+                // Create starting tile and move player to it.
                 getStartingLocation(out startX, out startY);
                 createTile(startX, startY, startTile);
                 movePlayer(startX, startY);
 
+                // Loop through all epochs.
                 for (int i = 0; i < episodes; i++)
                 {
                     episodesCount = i;
                     float x = startX;
                     float y = startY;
 
+                    // Keep going until we hit a terminal state (goal or out of map).
                     while (!isTerminalState(x, y))
                     {
+                        // Next action that the player should do.
                         int actionIndex = getNextAction(x, y, epsilon);
 
                         float oldX = x;
@@ -303,24 +329,28 @@ public class GridControllerContinuos : MonoBehaviour
                         getNextLocation(oldX, oldY, actionIndex, out x, out y);
                         movePlayer(x, y);
 
+                        // Reward for moving to that location.
                         int reward = GetReward();
 
                         acumaltedReward += reward;
 
                         Debug.Log(acumaltedReward);
 
+                        // We reached the goal.
                         if (reward == 100)
                         {
                             winsCount += 1;
                             winsPercentageCount = (float)Math.Round((winsCount / episodesCount) * 100, 2);
                         }
 
+                        // Get the new q value for the action we did.
                         float oldQValue = GetQValue(oldX, oldY, actionIndex);
 
                         float temporalDifference = reward + (discountFactor * GetQValueMax(x, y)) - oldQValue;
 
                         float newQValue = oldQValue + (learningRate * temporalDifference);
 
+                        // Train the gaussian surface that corresponds to the action we did. 
                         gaussArray[actionIndex].trainGaussSurface(oldX, oldY, alpha, sigma, centers, newQValue);
 
                         yield return new WaitForSeconds(delay);
@@ -333,24 +363,30 @@ public class GridControllerContinuos : MonoBehaviour
         }
     }
 
+    // Trainning process starting on random tiles each epoch.
     IEnumerator TrainQLearningStartRandom()
     {
         while (true)
         {
+            // When play btton is pressed.
             if (startTraining)
             {
+                // Loop through all epochs.
                 for (int i = 0; i < episodes; i++)
                 {
                     float x;
                     float y;
 
+                    // Get random starting location and move player to it.
                     getStartingLocation(out x, out y);
                     movePlayer(x, y);
 
                     episodesCount = i;
 
+                    // Keep going until we hit a terminal state (goal or out of map).
                     while (!isTerminalState(x, y))
                     {
+                        // Next action that the player should do.
                         int actionIndex = getNextAction(x, y, epsilon);
 
                         float oldX = x;
@@ -359,24 +395,28 @@ public class GridControllerContinuos : MonoBehaviour
                         getNextLocation(oldX, oldY, actionIndex, out x, out y);
                         movePlayer(x, y);
 
+                        // Reward for moving to that location.
                         int reward = GetReward();
 
                         acumaltedReward += reward;
 
                         Debug.Log(acumaltedReward);
 
+                        // We reached the goal.
                         if (reward == 100)
                         {
                             winsCount += 1;
                             winsPercentageCount = (float)Math.Round((winsCount / episodesCount) * 100, 2);
                         }
 
+                        // Get the new q value for the action we did.
                         float oldQValue = GetQValue(oldX, oldY, actionIndex);
 
                         float temporalDifference = reward + (discountFactor * GetQValueMax(x, y)) - oldQValue;
 
                         float newQValue = oldQValue + (learningRate * temporalDifference);
 
+                        // Train the gaussian surface that corresponds to the action we did. 
                         gaussArray[actionIndex].trainGaussSurface(oldX, oldY, alpha, sigma, centers, newQValue);
 
                         yield return new WaitForSeconds(delay);
